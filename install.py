6c8325f474271f679738d85a6b8929ec570152f8
James S McMurran <james@mcmurran.org>
2023-08-18 12:17:07 -0400

    Update install.py
    Speed up the install check making the boot up faster.

--- install.py
+++ install.py
@@ -1,9 +1,11 @@
+
 import launch
 import os
 import pkg_resources
 import sys
 from tqdm import tqdm
 import urllib.request
+import asyncio
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
@@ -12,43 +14,36 @@ model_url = "https://huggingface.co/henryruhs/roop/resolve/main/inswapper_128.on
 model_name = os.path.basename(model_url)
 model_path = os.path.join(models_dir, model_name)
 
-def download(url, path):
+async def download(url, path):
     request = urllib.request.urlopen(url)
     total = int(request.headers.get('Content-Length', 0))
     with tqdm(total=total, desc='Downloading', unit='B', unit_scale=True, unit_divisor=1024) as progress:
         urllib.request.urlretrieve(url, path, reporthook=lambda count, block_size, total_size: progress.update(block_size))
 
-if not os.path.exists(models_dir):
-    os.makedirs(models_dir)
-
-if not os.path.exists(model_path):
-    download(model_url, model_path)
-
-print("Checking roop requirements")
-with open(req_file) as file:
-    for package in file:
-        try:
-            python = sys.executable
+async def check_and_install_packages():
+    to_install = []
+    with open(req_file) as file:
+        for package in file:
             package = package.strip()
-
             if not launch.is_installed(package):
-                print(f"Install {package}")
-                launch.run_pip(
-                    f"install {package}", f"sd-webui-roop requirement: {package}"
-                )
+                to_install.append(package)
             elif "==" in package:
                 package_name, package_version = package.split("==")
                 installed_version = pkg_resources.get_distribution(package_name).version
                 if installed_version != package_version:
-                    print(
-                        f"Install {package}, {installed_version} vs {package_version}"
-                    )
-                    launch.run_pip(
-                        f"install {package}",
-                        f"sd-webui-roop requirement: changing {package_name} version from {installed_version} to {package_version}",
-                    )
+                    to_install.append(package)
+    if to_install:
+        packages_str = " ".join(to_install)
+        launch.run_pip(f"install {packages_str}", f"sd-webui-roop requirements: {packages_str}")
 
-        except Exception as e:
-            print(e)
-            print(f"Warning: Failed to install {package}, roop will not work.")
-            raise e
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+async def main():
+    tasks = []
+    if not os.path.exists(model_path):
+        tasks.append(asyncio.create_task(download(model_url, model_path)))
+    tasks.append(asyncio.create_task(check_and_install_packages()))
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
